db6629b211d5ad9d20ed01fd839e6e925e8d0e92
SIGUI Kessé Emmanuel (https://sikessem.com) <developer@sikessem.com>
2021-10-17 00:13:36 +0200

    :hammer: program: check the command args

--- src/Program.php
+++ src/Program.php
@@ -6,6 +6,19 @@ class Program {
     public function __construct(protected string $wdir) {}
 
     public function execute(int $argc, array $argv): void {
-        echo 'Setup your PHP App' . PHP_EOL;
+        if ( $argc > 1) {
+            switch ($argv[1]) {
+                case '-c':
+                    fprintf(STDOUT, 'We are going to compile file from %s' . PHP_EOL, $this->wdir);
+                    break;
+                
+                default:
+                    fprintf(STDERR, 'Unknow command: %s' . PHP_EOL, implode(' ', $argv));
+                    exit(1);
+            }
+
+        } else {
+            echo 'Setup your PHP App' . PHP_EOL;
+        }
     }
 }
